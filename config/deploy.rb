77c9518a94f339346063526802411ff5157b5a6a
ckhsponge <chris@spongecell.com>
2008-10-05 18:49:58 -0700

    more static

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,26 @@ namespace :deploy do
   end
 end
 
+namespace :static do
+  task :start, :roles => :app do
+    #run "cd /home/44693/domains/pokerpronto.com"
+    run "rm /home/44693/domains/pokerpronto.com/html;true"
+    #run "mkdir /home/44693/domains/pokerpronto.com/html"
+    run "cp -r /home/44693/containers/rails/pokerpronto/current/public /home/44693/domains/pokerpronto.com"
+    run "mv /home/44693/domains/pokerpronto.com/public /home/44693/domains/pokerpronto.com/html"
+    run "mv /home/44693/domains/pokerpronto.com/html/static.html /home/44693/domains/pokerpronto.com/html/index.html"
+    run "rm /home/44693/domains/pokerpronto.com/html/.htaccess;true"
+    run "rm /home/44693/domains/pokerpronto.com/html/dispatch.*;true"
+    mt_stop
+  end
+  
+  task :stop,  :roles => :app do
+    run "rm -rf /home/44693/domains/pokerpronto.com/html; true"
+    mt_create_link
+    mt_start
+  end
+end
+
 task :init_log_dir do
   run "mkdir -p /home/#{site}/containers/rails/#{application}/shared"
   run "mkdir -p /home/#{site}/containers/rails/#{application}/shared/log"
